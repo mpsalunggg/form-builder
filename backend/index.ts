df5fc78f867086d87d3b082f651d4ba88509d68f
mpsalunggg <putrasatria893@gmail.com>
2024-05-22 08:55:04 +0800

    chore: change response base endpoint

--- backend/index.ts
+++ backend/index.ts
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Setup Backend')
+  res.json({ message: 'Success hit' })
 })
 
 app.use('/api', taskRoutes)
